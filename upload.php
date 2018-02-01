27c008548f4d388f1827f18e4845699b935ff304
Tiago Matana <tiago.matana@gmail.com>
2018-02-01 14:37:54 -0200

    Identação do código

--- upload.php
+++ upload.php
@@ -1,8 +1,9 @@
 <?php
     if(!empty($_FILES['file'])){
+        $defaultPath = '.images/';
         $ext = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
-                $image = time().'.'.$ext;
-                move_uploaded_file($_FILES["file"]["tmp_name"], '.images/'.$image);
+        $image = time().'.'.$ext;
+        move_uploaded_file($_FILES["file"]["tmp_name"], $defaultPath.$image);
         echo $image." successfully uploaded";
     }else{
         echo "Invalid File or Empty File";
