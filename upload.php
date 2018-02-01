3733aa7b93e7c18e2c0ebf0690489caeb75fcb29
Tiago Matana <tiago.matana@gmail.com>
2018-02-01 14:29:44 -0200

    alteração no nome da pasta inicial

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
     if(!empty($_FILES['file'])){
         $ext = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
                 $image = time().'.'.$ext;
-                move_uploaded_file($_FILES["file"]["tmp_name"], 'images/'.$image);
+                move_uploaded_file($_FILES["file"]["tmp_name"], '.images/'.$image);
         echo $image." successfully uploaded";
     }else{
         echo "Invalid File or Empty File";
